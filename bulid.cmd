c3d3a6d491a9888cbc1a9814c2ac79aef32d3ea4
sdvcn <root@DESKTOP-FGQ95VR.localdomain>
2019-03-10 23:16:26 +0800

    add bulid

---
+++ bulid.cmd
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set OUTDIR=.\bulid
+set CHIP=STC12XX5624AD
+set OBJ=.\bulid
+
+set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
+set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
+
+rem --ide=hitide
+set ARGS= -Bs --time --summary=all --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ%% 
+
+
+c51 %ARGS% %1
+endlocal
