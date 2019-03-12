59555901b01295f3e1a80a1d86102a173b40b0f9
sdvcn <root@DESKTOP-M1T8CGV.localdomain>
2019-03-12 13:15:19 +0800

    stc 89

---
+++ include/stc89.h
@@ -0,0 +1,15 @@
+#ifndef _STC89_H_
+#define _STC89_H_
+
+static volatile unsigned char	P4	= 0xe8;
+
+static volatile bit	P40	@ 0xe8 ^ 0;
+static volatile bit	P41	@ 0xe8;
+static volatile bit	P42	@ 0xe8;
+static volatile bit	P43	@ 0xe8;
+static volatile bit	P44	@ 0xe8;
+static volatile bit	P45	@ 0xe8;
+static volatile bit	P46	@ 0xe8;
+static volatile bit	P47	@ 0xe8;
+
+#endif
