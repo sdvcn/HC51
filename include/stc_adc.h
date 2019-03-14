a345d373c73da6920ce03c12fadfac2149214359
sdvcn <sdvcn@126.com>
2019-03-14 15:10:06 +0800

    fix
    去掉中断部分

--- include/stc_adc.h
+++ include/stc_adc.h
@@ -1,9 +1,7 @@
 #ifndef _STC_ADC_H_
 #define _STC_ADC_H_
 
-
 extern void Adc_Init(unsigned char mask);
 extern unsigned short GetResult(unsigned char ch);
-
 
 #endif
--- src/stc_adc.c
+++ src/stc_adc.c
@@ -19,30 +19,6 @@
 
 
 unsigned char ADCMask;
-#ifdef INTER_ADC 
-#define NCh(_v) do{}while(!(ADCMask & (1u << (++_v &= ~0xf8))))
-//#define GetResult(_v) ADCres[(_v & (~0xf8))]
-unsigned short ADCres[ADCCH];
-
-void adc_isr() interrupt 5
-{
-    ///清除中断
-    ADC_CONTR &= ~ADC_FLAG ;
-
-    unsigned char ch = ADC_CONTR & (~0xf8);
-    ADCres[ch] = (ADC_DATA << 2) | ADC_LOW2;
-
-    NCh(ch);
-
-    ADC_CONTR = (ADC_CONTR & (~0x07) ) | ADC_START | ch;
-}
-unsigned short GetResult(unsigned char ch)
-{
-    ch &= ~0xf8;
-    return ADCres[ch];
-}
-
-#else
 
 unsigned short GetResult(unsigned char ch)
 {
@@ -56,9 +32,6 @@ unsigned short GetResult(unsigned char ch)
     ADC_CONTR &= ~ADC_FLAG;
     return (ADC_DATA << 2) | ADC_LOW2;
 }
-
-	
-#endif
 
 void Adc_Init(unsigned char mask)
 {
@@ -76,13 +49,54 @@ void Adc_Init(unsigned char mask)
     //设置通道IO开漏
     P1 = P1M0 = P1M1 = ADCMask;
     ADC_DATA = 0x00;
-#ifdef INTER_ADC 
-    NCh(ch);
-    ADC_CONTR =ADC_POWER|ADC_SPEED9 | ADC_START | ch;
-#else
     ADC_CONTR =ADC_POWER|ADC_SPEED9;
-#endif
     DelayMs(1);
 }
 
+/*
+    //ISR
+#define NCh(_v) do{}while(!(ADCMask & (1u << (++_v &= ~0xf8))))
+unsigned short ADCres[ADCCH];
 
+void adc_isr() interrupt 5
+{
+    ///清除中断
+    ADC_CONTR &= ~ADC_FLAG ;
+
+    unsigned char ch = ADC_CONTR & (~0xf8);
+    ADCres[ch] = (ADC_DATA << 2) | ADC_LOW2;
+
+    NCh(ch);
+
+    ADC_CONTR = (ADC_CONTR & (~0x07) ) | ADC_START | ch;
+}
+
+void Adc_InitIsr(unsigned char mask)
+{
+    unsigned char ch;
+    //卸载之前状态
+    ADC_CONTR = 0x00;
+    if(ADCMask){
+        P1M0 &= ~ ADCMask;
+        P1M1 &= ~ ADCMask;
+    }
+
+    ADCMask = mask;
+    ///关闭adc
+    if(ADCMask == 0x00) return;
+    //设置通道IO开漏
+    P1 = P1M0 = P1M1 = ADCMask;
+    ADC_DATA = 0x00;
+
+    NCh(ch);
+    ADC_CONTR =ADC_POWER|ADC_SPEED9 | ADC_START | ch;
+    DelayMs(1);
+}
+
+unsigned short GetResultIsr(unsigned char ch)
+{
+    ch &= ~0xf8;
+    return ADCres[ch];
+}
+
+*/
