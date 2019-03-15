c987a3beaaade8394215cfe03f433e1dbf7c1239
sdvcn <sdvcn@126.com>
2019-03-15 13:14:44 +0800

    增加测试编译

--- bulid.cmd
+++ bulid.cmd
@@ -4,12 +4,14 @@ setlocal
 set OUTDIR=.\bulid
 set CHIP=STC12XX5624AD
 set OBJ=.\bulid
+set INC=.\include
+set LIB=.\bulid\stc12X56.lpp
 
 set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
 set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
 
-set ARGS= -Bs --time --summary=all --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ%
+set ARGS= -Bs --time --summary=all -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ%
 
 
-c51 %ARGS% %1
+c51 %ARGS% %1 %LIB%
 endlocal
