f77d6fbf523cbd13af311a76ebd99f2672b015dd
sdvcn <sdvcn@126.com>
2019-03-20 17:01:40 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,17 +1,10 @@
 #include <8051.h>
 #include <stcmcu.h>
 
+
 void main()
 {
-	unsigned short v1;
-	unsigned short v2;
-	unsigned char c1;
-	unsigned char c2;
 
-	v1 = 0x1020;
-	c1 = v1;
-	c2 = v1 >> 8;
-	v2 = 0x1020;
 
 	while(1);
 }
