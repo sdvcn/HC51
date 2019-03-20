d9109b0e7ab957698e4314c4de742bd6781f8908
sdvcn <sdvcn@126.com>
2019-03-20 16:18:15 +0800

    Update stc12x56_misc.c
    更新延时函数

--- src/stc12x56_misc.c
+++ src/stc12x56_misc.c
@@ -5,22 +5,22 @@
 #include <stcmcu.h>
 
 //实现延时功能
-void DelayUs_Y1(unsigned short v)
+void DelayUs_Y1(ushort v)
 {
-	
+	_delay(v);
 }
 
-void DelayUs_Y3(unsigned short v)
+void DelayUs_Y3(ushort v)
 {
-	
+	_delay(v);
 }
 
-void DelayUs_Y5(unsigned short v)
+void DelayUs_Y5(ushort v)
 {
-	
+	_delay(v);
 }
 
-void DelayUs_Y6(unsigned short v)
+void DelayUs_Y6(ushort v)
 {
-	
+	_delay(v);
 }
