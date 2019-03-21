2f4172036b45bc6103ade63fc5c8b51ba2a1dec9
sdvcn <sdvcn@126.com>
2019-03-21 16:21:05 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,39 @@
 #include <8051.h>
 #include <stcmcu.h>
 
+typedef void (* LFun)(char* s);
+
+struct{
+	unsigned v1:4;
+	unsigned v2:4;
+}tss;
+
+struct OP
+{
+	unsigned char mode;
+	unsigned char len;
+	unsigned char* ptr;
+};
+void rr(char *s)
+{
+	if(P0){
+		NOP();
+	}
+	NOP();
+}
+
 
 void main()
 {
+	LFun ff1 = rr;
 
+	ushort v1 = &rr;
+	if(v1){
+		P0 = v1;
+		P1 = v1 >> 8;
+	}
+
+	ff1("sss");
 
 	while(1);
 }
