b9030489adf9bdc80f2eebc19820892a2bc8a2db
sdvcn <sdvcn@126.com>
2019-03-21 21:46:58 +0800

    Update bulid.cmd
    修改编译选项

--- bulid.cmd
+++ bulid.cmd
@@ -5,14 +5,14 @@ set OUTDIR=.\bulid
 set CHIP=STC12XX5624AD
 set OBJ=.\bulid
 set INC=.\include
-set LIB=.\bulid\stc12X56.lpp
+set LIB=.\src\stc*.c
 
 set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
 set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
 
-set ARGS= -Bs  -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ%
+set ARGS= -Bs  -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ% %LIB%
 
 
-c51 -S  %ARGS% %1 %LIB%
-c51 --time --summary=all %ARGS% %1 %LIB%
+rem c51 -C  %ARGS% %LIB% %1
+c51 --time --summary=all %ARGS% %1
 endlocal
