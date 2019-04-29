7d78cdbf3650662c49af68a40a0bee25c1775f1b
sdvcn <sdvcn@126.com>
2019-04-29 11:17:44 +0800

    Update bulid.cmd
    fix ext

--- bulid.cmd
+++ bulid.cmd
@@ -5,13 +5,16 @@ set OUTDIR=.\bulid
 set CHIP=STC8A8K60S4A12
 set OBJ=.\bulid
 set INC=.\include
-set LIB=.\src\stc*.c
+set LIB=.\src\stc*.c .\src\ext\*.c 
 
 set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
 set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
 
 set ARGS= -Bs  -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ% %LIB%
 
+echo //////////////////////////////////////////////////////////////////////
+echo ChipInfo = %CHIP%
+echo //////////////////////////////////////////////////////////////////////
 
 rem c51 -C  %ARGS% %LIB% %1
 c51 --time --summary=all %ARGS% %1
