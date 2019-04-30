651e088d11fc2d94dc5acdb3b1d0fc8c47b0bc66
sdvcn <sdvcn@126.com>
2019-04-30 16:21:16 +0800

    Update stc8.h
    更新寄存器定义

--- include/stc8.h
+++ include/stc8.h
@@ -16,6 +16,19 @@ static volatile unsigned char	P0M0	@	0x94;
 /// P2端口配置寄存器
 static volatile unsigned char	P2M1	@	0x95;
 static volatile unsigned char	P2M0	@	0x96;
+/// 串口2寄存器
+static volatile unsigned char	S2CON	@	0x9A;
+static volatile unsigned char	S2BUF	@	0x9B;
+
+static volatile unsigned char	SADDR	@	0xA9;
+static volatile unsigned char	WKTCL	@	0xAA;
+static volatile unsigned char	WKTCH	@	0xAB;
+static volatile unsigned char	S3CON	@	0xAC;
+static volatile unsigned char	S3BUF	@	0xAD;
+static volatile unsigned char	TA		@	0xAE;
+static volatile unsigned char	IE2		@	0xAF;
+
+
 /// P3端口配置寄存器
 static volatile unsigned char	P3M1	@	0xB1;
 static volatile unsigned char	P3M0	@	0xB2;
@@ -55,18 +68,72 @@ static volatile unsigned char	P6M0	@	0xCC;
 
 //ISP 命名兼容
 static volatile unsigned char	ISP_DATA	@	0xC2;
-static volatile unsigned int	ISP_ADDR	@	0xC3;		//fix
+static volatile unsigned short	ISP_ADDR	@	0xC3;		//fix
 static volatile unsigned char	ISP_ADDRH	@	0xC3;
 static volatile unsigned char	ISP_ADDRL	@	0xC4;
 static volatile unsigned char	ISP_CMD	@	0xC5;
 static volatile unsigned char	ISP_TRIG	@	0xC6;
 static volatile unsigned char	ISP_CONTR	@	0xC7;
 
+
+
+/// T2定时器
+static volatile unsigned char	T2H		@	0xD6;
+static volatile unsigned char	T2L		@	0xD7;
+
 /// P7端口配置寄存器
 static volatile unsigned char	P7M1	@	0xE1;
 static volatile unsigned char	P7M0	@	0xE2;
+// Port 4 P4
+static volatile unsigned char   P4  _at_ 0xC0;
+//static volatile SFR_BITS	P4_BITS @ 0xC0;
+static volatile bit	P40	@ 0xC0 ^ 0;
+static volatile bit	P41	@ 0xC0 ^ 1;
+static volatile bit	P42	@ 0xC0 ^ 2;
+static volatile bit	P43	@ 0xC0 ^ 3;
+static volatile bit	P44	@ 0xC0 ^ 4;
+static volatile bit	P45	@ 0xC0 ^ 5;
+static volatile bit	P46	@ 0xC0 ^ 6;
+static volatile bit	P47	@ 0xC0 ^ 7;
+
+// Port 5 P5
+//static volatile SFR_BITS	P5_BITS @ 0xC8;
+static volatile unsigned char   P5  _at_ 0xC8;
+static volatile bit	P50	@ 0xC8 ^ 0;
+static volatile bit	P51	@ 0xC8 ^ 1;
+static volatile bit	P52	@ 0xC8 ^ 2;
+static volatile bit	P53	@ 0xC8 ^ 3;
+static volatile bit	P54	@ 0xC8 ^ 4;
+static volatile bit	P55	@ 0xC8 ^ 5;
+static volatile bit	P56	@ 0xC8 ^ 6;
+static volatile bit	P57	@ 0xC8 ^ 7;
+
+/// P6 IO组
+//static volatile SFR_BITS	P6_BITS @ 0xE8;
+static volatile unsigned char   P6  _at_ 0xE8;
+static volatile bit	P60	@ 0xE8 ^ 0;
+static volatile bit	P61	@ 0xE8 ^ 1;
+static volatile bit	P62	@ 0xE8 ^ 2;
+static volatile bit	P63	@ 0xE8 ^ 3;
+static volatile bit	P64	@ 0xE8 ^ 4;
+static volatile bit	P65	@ 0xE8 ^ 5;
+static volatile bit	P66	@ 0xE8 ^ 6;
+static volatile bit	P67	@ 0xE8 ^ 7;
+
+// Port 7 P7
+//static volatile SFR_BITS	P7_BITS @ 0xF8;
+static volatile unsigned char   P7  _at_ 0xF8;
+static volatile bit	P70	@ 0xF8 ^ 0;
+static volatile bit	P71	@ 0xF8 ^ 1;
+static volatile bit	P72	@ 0xF8 ^ 2;
+static volatile bit	P73	@ 0xF8 ^ 3;
+static volatile bit	P74	@ 0xF8 ^ 4;
+static volatile bit	P75	@ 0xF8 ^ 5;
+static volatile bit	P76	@ 0xF8 ^ 6;
+static volatile bit	P77	@ 0xF8 ^ 7;
 
 
+//// 内存中预制
 idata volatile unsigned short Bandgap   @ 0xEF;
 idata volatile unsigned char STCID[7]	@ 0xF1;
 idata volatile unsigned short STC32K	@ 0xF8;
