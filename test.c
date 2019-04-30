7108d0f932e9611fa97afbe34c2a97a41c0a1153
sdvcn <sdvcn@126.com>
2019-04-30 17:15:59 +0800

    tmp

--- test.c
+++ test.c
@@ -10,9 +10,11 @@
 
 void uSEND(unsigned char s)
 {
-	SBUF = s;
-	while(!TI);
-	TI = 0;
+	S2BUF = s;
+	while(!(S2CON & BIT(1)));
+	ClearBIT(S2CON,1);
+	//while(!S2TI);
+	//S2TI = 0;
 }
 
 void uSENDs(const char* str,unsigned len)
@@ -33,27 +35,50 @@ void UartInit()
 	//busy = 0;
 }
 
+void UartInit2(void)		//115200bps@23.996MHz
+{
+	S2CON = 0x50;		//8位数据,可变波特率
+	AUXR |= 0x04;		//定时器2时钟为Fosc,即1T
+	T2L = 0xCC;		//设定定时初值
+	T2H = 0xFF;		//设定定时初值
+	AUXR |= 0x10;		//启动定时器2
+	//IE2 = 0x01;
+}
+
 void main()
 {
 	/// 初始化串口
-	ExtSfrSet(&P3PU,BIT(0)|BIT(1));
-	UartInit();
+	//ExtSfrSet(&P3PU,BIT(0)|BIT(1));
+	UartInit2();
+	//EA = 1;
 	
 	/// 设置端口寄存器 高阻态
-	P1M0 = 0x00;
-	P1M1 = (BIT(4)|BIT(5));
+	//P1M0 = 0x00;
+	//P1M1 = (BIT(4)|BIT(5));
 	/// 开启 P14 P15 内置上拉电阻
-	ExtSfrSet(&P1PU,0xff);
+	//ExtSfrSet(&P1PU,0xff);
 
-	P_SW2 |= 0x80;
+	P_SW2 = 0x80;
+	//P1PU = 0xff;
+	
 	///设置IO端口
 
 	uSENDs("Init:",5);
 	unsigned char sv1;
+	APDS9960_GestureSensor();
 		
 	do{
-		sv1 = APDS9960_ReadReg8(APDS9960_ID);
-		if(P10 == 0)uSEND(sv1);
-	}
-	while(1);
+		//uSENDs("Init:",5);
+		
+		if(P77 == 0){
+			//I2c_InitM(0x3f);
+			
+			//uSENDs("Init:",5);
+			//sv1 = APDS9960_ReadReg8(APDS9960_ID);
+			//uSEND(sv1);
+			//sv1 = APDS9960_ReadReg8(APDS9960_GFLVL);
+			//uSEND(sv1);
+			APDS9960_ReadGesture();
+		};
+	}while(1);
 }
