a3228423dfb059cbf634346c9fe12132b16229cb
sdvcn <sdvcn@126.com>
2019-05-01 14:57:50 +0800

    Update stc_i2c.c
    精简代码

--- src/stc_i2c.c
+++ src/stc_i2c.c
@@ -1,119 +1,85 @@
 #include <stcmcu.h>
-
-enum{
-    MSCMD_NONE    = 0b0000,
-    MSCMD_START   = 0b0001,
-    MSCMD_WRITE   = 0b0010,
-    MSCMD_RACK    = 0b0011,
-    MSCMD_READ    = 0b0100,
-    MSCMD_TACK    = 0b0101,
-    MSCMD_STOP    = 0b0110,
-};
+#include <i2c.h>
 //MSCMDEXT
+unsigned char I2c_GetBuf()
+{
+    return I2CRxD;
+}
 
-bool I2c_Busy()
+void I2c_SetBuf(unsigned char c)
+{
+    I2CTxD = c;
+}
+
+bit I2c_Busy()
 {
     return (I2CMSST & MSBUSY) ? 1ul:0ul;
 }
 
-bool I2c_CFG(unsigned char op)
+/// 检测收到NAck
+bit I2c_NAckStatus()
+{
+    return CheckBIT(I2CMSST,1);
+}
+
+bit I2c_CFG(unsigned char op)
 {
     if(I2CMSST & MSBUSY) return 0ul;
-    I2CCFG = op;
-    //I2CCFG = 0x00;
-    //_delay(10);
-    //I2CCFG = 0xe0;
-    I2CMSST = 0x00;
+    I2CCFG      = op;
+    I2CMSST     = 0x00;
+    I2CMSAUX    = 0x00;
     return 1ul;
 }
-
-void I2c_Wait()
+/// I2C 控制命令
+void I2c_Cmd(unsigned char cmd)
 {
-    //_delay(10);
+    I2CMSCR = ((I2CMSCR & ~0x0f)| (cmd & ~0xf0));
     while(!(I2CMSST & MSIF));
     I2CMSST &= ~MSIF;
-    //_delay(10);
 }
-
-void I2c_Start()
-{
-    I2CMSCR = MSCMD_START;
-    I2c_Wait();
-}
-
+/// 写入字符
 void I2c_Write(unsigned char c)
 {
     I2CTxD = c;
-    I2CMSCR = MSCMD_WRITE;
-    I2c_Wait();
+    I2c_Cmd(MSCMD_WRITE);
 }
-
-void I2c_RxAck()
-{
-    I2CMSCR = MSCMD_RACK;
-    I2c_Wait();
-}
-
+/// 读字符
 unsigned char I2c_Read()
 {
-    I2CMSCR = MSCMD_READ;
-    I2c_Wait();
+    I2c_Cmd(MSCMD_READ);
     return I2CRxD;
 }
-
-void I2c_TxAck()
+/// 发送应答
+void I2c_Ack(unsigned char nAck)
 {
-    I2CMSST = 0x00;
-    I2CMSCR = MSCMD_TACK;
-    I2c_Wait();
+    I2CMSST = (nAck?0x01:0x00);
+    I2c_Cmd(MSCMD_TACK);
 }
 
-void I2c_TxNAck()
+///写字符串 ???
+unsigned I2c_Writes(unsigned len,char* src)
 {
-    I2CMSST = 0x01;
-    I2CMSCR = MSCMD_TACK;
-    I2c_Wait();
-}
-
-void I2c_Stop()
-{
-    I2CMSCR = MSCMD_STOP;
-
-    I2c_Wait();
-    
-}
-
-
-
-
-/*
-bool I2c_Reads(unsigned char addr,unsigned char len,char* dst)
-{
-    if(I2CMSST & MSBUSY) return false;
-    I2c_Start();
-    I2c_Write(RAddr(addr));
-    I2c_RxAck();
-
+    unsigned ret = 0x00;
     while(len--){
-        *dst++ = I2c_Read();
-        I2c_TxAck();
-    }
-    I2c_Stop();
-    return true;
-}
-
-bool I2c_Writes(unsigned char addr,unsigned char len,char* src)
-{
-    if(I2CMSST & MSBUSY) return false;
-    I2c_Start();
-    I2c_Write(WAddr(addr));
-    I2c_RxAck();
-    while(len--){
+        I2c_Write(*src++);
+        ///发送ACK
+        //I2c_Ack(!len);
         
+        ret++;
+        I2c_RxAck();
+        if(I2c_NAckStatus()) break;
     }
-    
-
-
-    return true;
+    return ret;
 }
-*/
+///读字符串
+unsigned I2c_Reads(unsigned len,char* dst)
+{
+    unsigned ret = 0x00;
+    while(len--){
+        *dst++=I2c_Read();
+        ret++;
+        if(I2c_NAckStatus) break;
+        //if(CheckBIT(I2CMSST,1)) break;
+    }
+    return ret;
+}
