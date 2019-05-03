e85da9314609e7ce8ff6a553d0fed1cd2bb1ca59
sdvcn <sdvcn@126.com>
2019-05-03 09:29:21 +0800

    Update bulid.cmd
    增加Debug部分

--- bulid.cmd
+++ bulid.cmd
@@ -5,12 +5,13 @@ set OUTDIR=.\bulid
 set CHIP=STC8A8K60S4A12
 set OBJ=.\bulid
 set INC=.\include
+set OUTFILE=8051.hex
 set LIB=.\src\stc*.c .\src\ext\*.c 
 
 set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
 set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
-
-set ARGS= -Bs  -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ% %LIB%
+set DEFINE=-DDEBUG
+set ARGS=%DEFINE% -Bs  -I%INC% --chip=%CHIP% --outdir=%OUTDIR% --objdir=%OBJ% %LIB% -O%OUTFILE%
 
 echo //////////////////////////////////////////////////////////////////////
 echo ChipInfo = %CHIP%
