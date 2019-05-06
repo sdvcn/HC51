723c02456d53203cd7315901fc1861ac880668ea
sdvcn <sdvcn@126.com>
2019-05-07 04:37:34 +0800

    up

--- src/ext/apds9660.c
+++ src/ext/apds9660.c
@@ -8,6 +8,18 @@
 #include <i2c.h>
 #include <ext_debug.h>
 /**
+ * Apds-9960总线地址
+*/
+#ifndef APDS9960_ADDRES
+#define APDS9960_ADDRES 0x39
+#endif
+/**
+ * Apds-9960 总线速度
+*/
+#ifndef I2C_SPEED
+#define I2C_SPEED 0x30
+#endif
+/**
  * 9960基础定义
 */
 /* APDS-9960 register addresses */
@@ -88,18 +100,54 @@
 #define APDS9960_GFIFO_L        0xFE
 /// 手势 右
 #define APDS9960_GFIFO_R        0xFF
-/**
- * Apds-9960总线地址
-*/
-#ifndef APDS9960_ADDRES
-#define APDS9960_ADDRES 0x39
-#endif
-/**
- * Apds-9960 总线速度
-*/
-#ifndef I2C_SPEED
-#define I2C_SPEED 0x30
-#endif
+
+/* 启动使能 */
+/// 主电源
+#define APDS9960_PON            (1ul << 0)
+/// 环境光线
+#define APDS9960_AEN            (1ul << 1)
+/// 接近判定
+#define APDS9960_PEN            (1ul << 2)
+/// 等待延时
+#define APDS9960_WEN            (1ul << 3)
+/// 环境光线
+#define APSD9960_AIEN           (1ul << 4)
+/// 接近中断
+#define APDS9960_PIEN           (1ul << 5)
+/// 手势
+#define APDS9960_GEN            (1ul << 6)
+///
+//#define APDS9960_GVALID         0b00000001
+/* Default values */
+#define DEFAULT_ATIME           219     // 103ms
+#define DEFAULT_WTIME           246     // 27ms
+#define DEFAULT_PROX_PPULSE     0x87    // 16us, 8 pulses
+#define DEFAULT_GESTURE_PPULSE  0x89    // 16us, 10 pulses
+#define DEFAULT_POFFSET_UR      0       // 0 offset
+#define DEFAULT_POFFSET_DL      0       // 0 offset      
+#define DEFAULT_CONFIG1         0x60    // No 12x wait (WTIME) factor
+#define DEFAULT_LDRIVE          LED_DRIVE_100MA
+#define DEFAULT_PGAIN           PGAIN_4X
+#define DEFAULT_AGAIN           AGAIN_4X
+#define DEFAULT_PILT            0       // Low proximity threshold
+#define DEFAULT_PIHT            50      // High proximity threshold
+#define DEFAULT_AILT            0xFFFF  // Force interrupt for calibration
+#define DEFAULT_AIHT            0
+#define DEFAULT_PERS            0x11    // 2 consecutive prox or ALS for int.
+#define DEFAULT_CONFIG2         0x01    // No saturation interrupts or LED boost  
+#define DEFAULT_CONFIG3         0       // Enable all photodiodes, no SAI
+#define DEFAULT_GPENTH          40      // Threshold for entering gesture mode
+#define DEFAULT_GEXTH           30      // Threshold for exiting gesture mode    
+#define DEFAULT_GCONF1          0x40    // 4 gesture events for int., 1 for exit
+#define DEFAULT_GGAIN           GGAIN_4X
+#define DEFAULT_GLDRIVE         LED_DRIVE_100MA
+#define DEFAULT_GWTIME          GWTIME_2_8MS
+#define DEFAULT_GOFFSET         0       // No offset scaling for gesture mode
+#define DEFAULT_GPULSE          0xC9    // 32us, 10 pulses
+#define DEFAULT_GCONF3          0       // All photodiodes active during gesture
+#define DEFAULT_GIEN            0       // Disable gesture interrupts
+
+
 
 typedef struct{
     unsigned char mU;
@@ -182,14 +230,20 @@ unsigned char APDS9960_ReadReg8(unsigned char reg)
  
     unsigned char val = 0x00;
     APDS9960_ReadReg(reg,1,&val);
+
+    DLOGINT(APDS9960_ReadReg8,val);
     return val;
 }
 /// 寄存器读取 unsigned short
 unsigned short APDS9960_ReadReg16(unsigned char reg)
 {
+    unsigned short ret = 0x00;
     unsigned char val[2] = {0x00};
     APDS9960_ReadReg(reg,2,&val);
-    return ((val[1] << 7)|val[0]);
+    ret = ((val[1] << 7)|val[0]);
+
+    DLOGINT(APDS9960_ReadReg16,ret);
+    return ret;
 }
 /// 寄存器写 unsigned char
 void APDS9960_WriteReg8(unsigned char reg,unsigned char val)
@@ -253,22 +307,17 @@ unsigned char APDS9960_Check()
 }
 /// 获取开启模式
 
+//初始化
 void APDS9960_Init()
 {
     unsigned char mid=0x00;
-    DLOG("APDS9960_Init:");
+    DLOG("APDS9960_Init");
     //
     mid = APDS9960_ReadReg8(APDS9960_ID);
     //DLOGINT(APDS9960_Init,mid);
-    DLOG("4");
     mid = APDS9960_ReadReg8(APDS9960_ENABLE);
-    DLOG("5");
     //关闭所有
     APDS9960_WriteReg8(APDS9960_ENABLE,0x00);
-    DLOG("6");
-#ifdef DEBUG
-    //printf("%s\t%d:APDS9960_ENABLE(%d):%d",__FILE__,__LINE__,APDS9960_ENABLE,APDS9960_ReadReg8(APDS9960_ENABLE));
-#endif
 }
 
 void APDS9960_GestureSensor()
--- src/ext/apds9660.h
+++ src/ext/apds9660.h
@@ -9,52 +9,6 @@
 #define APDS9960_ID_2           0x9C 
 
 
-/* 启动使能 */
-/// 主电源
-#define APDS9960_PON            (1ul << 0)
-/// 环境光线
-#define APDS9960_AEN            (1ul << 1)
-/// 接近判定
-#define APDS9960_PEN            (1ul << 2)
-/// 等待延时
-#define APDS9960_WEN            (1ul << 3)
-/// 环境光线
-#define APSD9960_AIEN           (1ul << 4)
-/// 接近中断
-#define APDS9960_PIEN           (1ul << 5)
-/// 手势
-#define APDS9960_GEN            (1ul << 6)
-///
-//#define APDS9960_GVALID         0b00000001
-/* Default values */
-#define DEFAULT_ATIME           219     // 103ms
-#define DEFAULT_WTIME           246     // 27ms
-#define DEFAULT_PROX_PPULSE     0x87    // 16us, 8 pulses
-#define DEFAULT_GESTURE_PPULSE  0x89    // 16us, 10 pulses
-#define DEFAULT_POFFSET_UR      0       // 0 offset
-#define DEFAULT_POFFSET_DL      0       // 0 offset      
-#define DEFAULT_CONFIG1         0x60    // No 12x wait (WTIME) factor
-#define DEFAULT_LDRIVE          LED_DRIVE_100MA
-#define DEFAULT_PGAIN           PGAIN_4X
-#define DEFAULT_AGAIN           AGAIN_4X
-#define DEFAULT_PILT            0       // Low proximity threshold
-#define DEFAULT_PIHT            50      // High proximity threshold
-#define DEFAULT_AILT            0xFFFF  // Force interrupt for calibration
-#define DEFAULT_AIHT            0
-#define DEFAULT_PERS            0x11    // 2 consecutive prox or ALS for int.
-#define DEFAULT_CONFIG2         0x01    // No saturation interrupts or LED boost  
-#define DEFAULT_CONFIG3         0       // Enable all photodiodes, no SAI
-#define DEFAULT_GPENTH          40      // Threshold for entering gesture mode
-#define DEFAULT_GEXTH           30      // Threshold for exiting gesture mode    
-#define DEFAULT_GCONF1          0x40    // 4 gesture events for int., 1 for exit
-#define DEFAULT_GGAIN           GGAIN_4X
-#define DEFAULT_GLDRIVE         LED_DRIVE_100MA
-#define DEFAULT_GWTIME          GWTIME_2_8MS
-#define DEFAULT_GOFFSET         0       // No offset scaling for gesture mode
-#define DEFAULT_GPULSE          0xC9    // 32us, 10 pulses
-#define DEFAULT_GCONF3          0       // All photodiodes active during gesture
-#define DEFAULT_GIEN            0       // Disable gesture interrupts
-
 //----------------------------------------
 extern unsigned char APDS9960_ReadReg8(unsigned char reg);
 extern unsigned short APDS9960_ReadReg16(unsigned char reg);
