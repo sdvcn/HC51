60aa122510c3cfa71351967c7bf4ff3d286b7fef
sdvcn <sdvcn@126.com>
2019-05-06 17:05:09 +0800

    Update apds9660.h
    预计作废

--- src/ext/apds9660.h
+++ src/ext/apds9660.h
@@ -1,87 +1,13 @@
+/**
+ * 废弃
+*/
 #ifndef _APDS9960_H_
 #define _APDS9960_H_
 
 /* Acceptable device IDs */
 #define APDS9960_ID_1           0xAB        //ADPS-9960
 #define APDS9960_ID_2           0x9C 
-/* APDS-9960 register addresses */
-#define APDS9960_ENABLE         0x80
-/// 环境光 adc 中断时间
-#define APDS9960_ATIME          0x81
-/// 延时
-#define APDS9960_WTIME          0x83
-/// 环境光阀值
-#define APDS9960_AILTL          0x84
-#define APDS9960_AILTH          0x85
 
-#define APDS9960_AIHTL          0x86
-#define APDS9960_AIHTH          0x87
-/// 接近中断阀值
-#define APDS9960_PILT           0x89
-#define APDS9960_PIHT           0x8B
-
-#define APDS9960_PERS           0x8C
-#define APDS9960_CONFIG1        0x8D
-/// 接近脉冲寄存器
-#define APDS9960_PPULSE         0x8E
-
-#define APDS9960_CONTROL        0x8F
-#define APDS9960_CONFIG2        0x90
-/// 驱动器编号
-#define APDS9960_ID             0x92
-/// 驱动器状态
-#define APDS9960_STATUS         0x93
-///
-#define APDS9960_CDATAL         0x94
-#define APDS9960_CDATAH         0x95
-/// 红通道数据
-#define APDS9960_RDATAL         0x96
-#define APDS9960_RDATAH         0x97
-/// 绿通道数据
-#define APDS9960_GDATAL         0x98
-#define APDS9960_GDATAH         0x99
-/// 蓝通道数据
-#define APDS9960_BDATAL         0x9A
-#define APDS9960_BDATAH         0x9B
-/// 接近通道数据
-#define APDS9960_PDATA          0x9C
-
-
-#define APDS9960_POFFSET_UR     0x9D
-#define APDS9960_POFFSET_DL     0x9E
-#define APDS9960_CONFIG3        0x9F
-#define APDS9960_GPENTH         0xA0
-#define APDS9960_GEXTH          0xA1
-#define APDS9960_GCONF1         0xA2
-#define APDS9960_GCONF2         0xA3
-#define APDS9960_GOFFSET_U      0xA4
-#define APDS9960_GOFFSET_D      0xA5
-#define APDS9960_GOFFSET_L      0xA7
-#define APDS9960_GOFFSET_R      0xA9
-#define APDS9960_GPULSE         0xA6
-#define APDS9960_GCONF3         0xAA
-/// 手势设定 
-#define APDS9960_GCONF4         0xAB
-/// 手势寄存器缓冲数量
-#define APDS9960_GFLVL          0xAE
-/// 手势状态
-#define APDS9960_GSTATUS        0xAF
-/// 强制中断
-#define APDS9960_IFORCE         0xE4
-/// 接近中断清除
-#define APDS9960_PICLEAR        0xE5
-/// ALS 通道中断清除
-#define APDS9960_CICLEAR        0xE6
-/// 全部非手势中断清除
-#define APDS9960_AICLEAR        0xE7
-/// 手势 上
-#define APDS9960_GFIFO_U        0xFC
-/// 手势 下
-#define APDS9960_GFIFO_D        0xFD
-/// 手势 左
-#define APDS9960_GFIFO_L        0xFE
-/// 手势 右
-#define APDS9960_GFIFO_R        0xFF
 
 /* 启动使能 */
 /// 主电源
@@ -159,10 +85,10 @@ extern void APDS9960_ReadGesture();
 
 //----------------------------------------
 /// 开启APDS-9960 I2c访问
-unsigned char ADPS9960_I2c_En();
+//unsigned char ADPS9960_I2c_En();
 
 /// 停止APDS-9960 I2c访问
-unsigned char ADPS9960_I2c_Di();
+//unsigned char ADPS9960_I2c_Di();
 
 void APDS9960_Init();
 
