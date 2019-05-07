c708751158654aefed6a86c69ed883173ef6c21a
sdvcn <sdvcn@126.com>
2019-05-07 10:16:15 +0800

    Update stcmcu.h
    更新说明

--- include/stcmcu.h
+++ include/stcmcu.h
@@ -23,10 +23,13 @@ typedef unsigned short	ushort;
 */
 ///位操作
 #define BIT(_Sv)	(1ul << _Sv)
+
 ///检查位
 #define CheckBIT(_addr,_bit) ((_addr & BIT(_bit))?1:0)
+
 ///清除位
 #define ClearBIT(_addr,_bit) (_addr &=~ BIT(_bit))
+
 ///设置位
 #define SetBIT(_addr,_bit) (_addr |= BIT(_bit))
 
