9e17b0b4f16cbfe9a1c172d9f3acf24220a0286f
sdvcn <sdvcn@126.com>
2019-05-09 16:25:04 +0800

    Update stc_debug.c
    更新 Console_DumpHex ,ASCII字符过滤

--- src/stc_debug.c
+++ src/stc_debug.c
@@ -9,6 +9,7 @@ void Console_DumpHex(size_t len,char* src)
 {
     unsigned char buf[16];
     unsigned char ln = 0x00;
+    printf("==Dump len(%d)\n",len);
 
     for (size_t l=0;l < len;)
     {
@@ -28,7 +29,11 @@ void Console_DumpHex(size_t len,char* src)
         }
         printf(" | ");
         for(unsigned char i=0;i<0x10;i++){
-            if(buf[i] == 0x00) buf[i] = 0x20;
+            ///过滤非ASCII字符 不支持双字节
+            buf[i] &= 0x7f;
+            /// 过滤控制符
+            if(buf[i] < 32) buf[i] = 0x20;
+            if(buf[i] == 0x7f) buf[i] = 0x20;
             printf(" %c",buf[i]);
         }
         printf(" |\n");
