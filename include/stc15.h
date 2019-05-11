892c1cac24357a4d1bab7a90ca0fca3ef36039a8
sdvcn <sdvcn@126.com>
2019-05-12 01:12:31 +0800

    Update stc15.h
    更新全集中断操作方法

--- include/stc15.h
+++ include/stc15.h
@@ -1,4 +1,19 @@
 #ifndef _STC15_H_
 #define _STC15_H_
 
+/**
+ * 取消自带全局EA定义
+ * 重新定义
+*/
+#ifdef ei()
+#undef ei()
+#endif
+
+#ifdef di()
+#undef di()
+#endif
+
+#define ei()    do{NOP();EA = 1;NOP();}while(0)
+#define di()    do{NOP();EA = 0;NOP();}while(0)
+
 #endif
