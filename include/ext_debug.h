c04936126829d2478de12249a6cc6c6daff8fe96
sdvcn <sdvcn@126.com>
2019-05-20 17:09:49 +0800

    Update ext_debug.h
    更新 assert

--- include/ext_debug.h
+++ include/ext_debug.h
@@ -5,22 +5,34 @@
 #ifndef __EXT_DEBUG_H__
 #define __EXT_DEBUG_H__
 #include <stdio.h>
+//#include <assert.h>       //不能调用
 //__FILE__
 //__LINE__
 
-#ifdef DEBUG
+#ifndef NDEBUG
+
+#ifndef	__mkstr__
+#define	__mkstr__(exp)	#exp
+#endif
+
 const char* Txt_Info="[%s:%d]\tInfo:%s\n";
 const char* Txt_Error="[%s:%d]\tError:%s\n";
 const char* Txt_I2Hex="[%s:%d]\tTracert:%s[%s:0x%2.2X]\n";
 
 #define DLOG(_v) printf(Txt_Info,__FILE__,__LINE__,_v);
 #define ELOG(_v) printf(Txt_Error,__FILE__,__LINE__,_v);
-//#define ELOGV(_f) printf(Txt_Error,__FILE__,__LINE__,_v);
 #define DLOGINT(_f,_v) printf(Txt_I2Hex,__FILE__,__LINE__,#_f,#_v,_v);
+
+#define	assert(exp)	if(!(exp)) {printf(Txt_Info, __FILE__,__LINE__, __mkstr__(exp));}
+
 #else
+
 #define DLOG(_v)
 #define ELOG(_v)
 #define DLOGINT(_f,_v)
+#define	assert(exp)
+
+
 
 #endif
 
