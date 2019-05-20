0e6cb1b7be52eb82cc874fbdcb4aa23de5db690c
sdvcn <sdvcn@126.com>
2019-05-21 00:24:27 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ set CHIP=STC8A8K60S4A12
 set OBJ=.\bulid
 set INC=.\include
 set OUTFILE=8051.hex
-set LIB=.\src\stc*.c .\src\ext\*.c 
+set LIB=.\src\*.c .\src\ext\*.c 
 
 set HTC_WARN_FORMAT=WARNING: file %f; line %l; column %c; %s
 set HTC_ERR_FORMAT=ERROR: file %f; line %l; column %c; %s
@@ -21,14 +21,14 @@ echo //////////////////////////////////////////////////////////////////////
 
 rem c51 -C  %ARGS% %LIB% %1
 IF %ERRORLEVEL% NEQ 0 goto enderr
-c51 -S -Q %ARGS% %1
+c51 -S -Q -P %ARGS% %*
 IF %ERRORLEVEL% NEQ 0 goto enderr
-c51 --time --summary=all %ARGS% %1
+c51 --time --summary=all %ARGS% %*
 IF %ERRORLEVEL% NEQ 0 goto enderr
 goto gend
 :enderr
 echo //////////////////////////////////////////////////////////////////////
-echo ERROR
+echo ERROT %ERRORLEVEL%
 echo //////////////////////////////////////////////////////////////////////
 :gend
 
