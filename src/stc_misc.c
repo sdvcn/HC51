3a68e92548985f32440d6d87d87f01e484d1f830
sdvcn <sdvcn@126.com>
2019-05-21 15:19:44 +0800

    Update stc_misc.c
    移动 stc8支持函数到stc_stc8.c

--- src/stc_misc.c
+++ src/stc_misc.c
@@ -4,11 +4,7 @@
 */
 #include <stcmcu.h>
 
-///
-#define EN_EXTSFR() SetBIT(P_SW2,EAXFR)
-#define Di_EXTSFR() ClearBIT(P_SW2,EAXFR)
-///地址直读
-#define GetVal(_addr) (*(volatile unsigned char xdata *)_addr)
+#include <ext_debug.h>
 
 /// HTC 对应开始地址 针对编译器
 interrupt void hid_intr(void) _at_ 0xfb
@@ -16,60 +12,4 @@ interrupt void hid_intr(void) _at_ 0xfb
 }
 
 
-/**
- * STC8 扩展寄存器调用
- * 禁止从中断处调用
-*/
 
-unsigned char ExtSfrGet8(size_t addr){
-	unsigned char r;
-    if(EaxFRST()){
-        r = GetVal(addr);
-        return r;
-    };
-
-    EN_EXTSFR();
-    r = GetVal(addr);
-    Di_EXTSFR();
-    return r;
-}
-
-extern void ExtSfrSet8(size_t addr,unsigned char v)
-{
-    if(EaxFRST()){
-        GetVal(addr) = v;
-        return;
-    };
-    EN_EXTSFR();
-    GetVal(addr) = v;
-    Di_EXTSFR();
-
-}
-
-unsigned short ExtSfrGet16(size_t addr){
-	unsigned short r;
-    if(EaxFRST()){
-        r = GetVal(addr);
-        return r;
-    };
-
-    EN_EXTSFR();
-    r = GetVal(addr);
-    Di_EXTSFR();
-    return r;
-}
-
-void ExtSfrSet16(size_t addr,unsigned short v){
-    if(EaxFRST()){
-        GetVal(addr) = v;
-        return;
-    };
-    EN_EXTSFR();
-    GetVal(addr) = v;
-    Di_EXTSFR();
-}
-
-//STC8 扩展
-#ifdef STC8
-
-#endif
