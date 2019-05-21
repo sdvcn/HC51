e9b17ffc816900f0089a07615f07ff6063f9494b
sdvcn <sdvcn@126.com>
2019-05-21 15:18:47 +0800

    Update ext_debug.h
    fix function

--- include/ext_debug.h
+++ include/ext_debug.h
@@ -19,9 +19,9 @@ const char* Txt_Info="[%s:%d]\tInfo:%s\n";
 const char* Txt_Error="[%s:%d]\tError:%s\n";
 const char* Txt_I2Hex="[%s:%d]\tTracert:%s[%s:0x%2.2X]\n";
 
-#define DLOG(_v) printf(Txt_Info,__FILE__,__LINE__,_v);
-#define ELOG(_v) printf(Txt_Error,__FILE__,__LINE__,_v);
-#define DLOGINT(_f,_v) printf(Txt_I2Hex,__FILE__,__LINE__,#_f,#_v,_v);
+#define DLOG(_v) printf(Txt_Info,__FILE__,__LINE__,_v)
+#define ELOG(_v) printf(Txt_Error,__FILE__,__LINE__,_v)
+#define DLOGINT(_f,_v) printf(Txt_I2Hex,__FILE__,__LINE__,#_f,#_v,_v)
 
 #define	assert(exp)	if(!(exp)) {printf(Txt_Info, __FILE__,__LINE__, __mkstr__(exp));}
 
