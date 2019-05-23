79473acacda1eee54a7fd0193ac083a3f9127cde
sdvcn <sdvcn@126.com>
2019-05-23 13:56:42 +0800

    Update stc_stc8.c
    增加系统时钟函数

--- src/stc_stc8.c
+++ src/stc_stc8.c
@@ -45,6 +45,76 @@ void free(void * p)
 {
 
 }
+#define IRCMST      0x01
+#define ENIRC       0x80
+#define SelectIrc24M    0x00;
+#define SelectIrc32K    0x03;
+
+
+
+
+void _SelectIrc24m()
+{
+    IRCRC = ExtSfrGet8(&IRC24M);
+    ExtSfrSet8(&IRC24MCR,ENIRC);
+    while(!(ExtSfrGet8(&IRC24MCR) & IRCMST));
+}
+
+void _SelectIrc32k()
+{
+    ExtSfrSet8(&IRC32KCR,ENIRC);
+    while(!(ExtSfrGet8(&IRC32KCR) & IRCMST));
+}
+
+/**
+ * 不对外部时钟进行验证
+ * 且转时会等待稳定
+*/
+void SetSystemClock(unsigned char op,unsigned char cdiv)
+{
+    unsigned char cksel = 0x00;
+
+    if((op& 0x03) == 0x00){
+        _SelectIrc24m();
+    }else if((op& 0x03) == 0x03) {
+        _SelectIrc32k();
+    }else{
+        // todo:外部晶振未实现
+    }
+    ExtSfrSet8(&CLKDIV,cdiv);
+    ExtSfrSet8(&CKSEL,op);
+
+    cksel = ExtSfrGet8(&CKSEL);
+    /**
+     * 关闭无用时钟源
+    */
+    if((cksel& 0x03) == 0x00){
+        //_SelectIrc24m();
+        ExtSfrSet8(&IRC32KCR,0x00);
+    }else if((cksel& 0x03) == 0x03) {
+        //_SelectIrc32k();
+        ExtSfrSet8(&IRC24MCR,0x00);
+    }else{
+        // todo:外部晶振未实现
+    }
+}
+
+
+unsigned long GetSystemClock()
+{
+    unsigned char cksel = ExtSfrGet8(&CKSEL);
+    unsigned char clkdiv = ExtSfrGet8(&CLKDIV);
+    unsigned long r;
+    if((cksel & 0x03) == 0x00 ) r = DEFCLK;
+    if((cksel & 0x03) == 0x01 ) return 0ul;
+    if((cksel & 0x03) == 0x02 ) return 0ul;
+    if((cksel & 0x03) == 0x03 ) r = 32000ul;
+
+    if (clkdiv) r /= clkdiv;                            // 系统分频
+
+    return r;
+}
+
 
 
 
@@ -55,30 +125,32 @@ void free(void * p)
 */
 
 /// 8位
-#define GetVal8(_addr) (*(xdata volatile unsigned char *)_addr)
+//#define GetVal8(_addr) (*(xdata volatile unsigned char *)_addr)
 /// 16位
-#define GetVal16(_addr) (*(xdata volatile unsigned short *)_addr)
+//#define GetVal16(_addr) (*(xdata volatile unsigned short *)_addr)
 
 /**
  * STC8 扩展寄存器调用
  * 禁止从中断处调用
 */
-unsigned char ExtSfrGet8(size_t addr){
+unsigned char _ExtSfrGet8(far volatile unsigned char* reg){
 	unsigned char r = 0x00;
-    DLOGINT(ExtSfrGet8,addr);
+    DLOGINT(ExtSfrGet8,reg);
+    assert((size_t)reg > 0xfdff);
     asm("push _P_SW2");
     asm("orl _P_SW2,#80h");
     asm("nop");
-    r = (*(volatile unsigned char xdata *)addr);
+    r = (*(volatile unsigned char xdata *)reg);
     asm("pop _P_SW2");
     DLOGINT(ExtSfrSet8,r);
     return r;
 }
 
-void ExtSfrSet8(size_t addr,unsigned char v)
+void _ExtSfrSet8(far volatile unsigned char* addr,unsigned char v)
 {
     DLOGINT(ExtSfrSet8,addr);
     DLOGINT(ExtSfrSet8,v);
+    assert((size_t)addr > 0xfdff);
     asm("push _P_SW2");
     asm("orl _P_SW2,#80h");
     asm("nop");
@@ -89,9 +161,10 @@ void ExtSfrSet8(size_t addr,unsigned char v)
     #endif
 }
 
-unsigned short ExtSfrGet16(size_t addr){
+unsigned short _ExtSfrGet16(far volatile unsigned short * addr){
 	unsigned short r = 0x00;
     DLOGINT(ExtSfrGet8,addr);
+    assert((size_t)addr > 0xfdff);
     asm("push _P_SW2");
     asm("orl _P_SW2,#80h");
     asm("nop");
@@ -101,9 +174,10 @@ unsigned short ExtSfrGet16(size_t addr){
     return r;
 }
 
-void ExtSfrSet16(size_t addr,unsigned short v){
+void _ExtSfrSet16(far volatile unsigned short * addr,unsigned short v){
     DLOGINT(ExtSfrSet16,addr);
     DLOGINT(ExtSfrSet16,v);
+    assert((size_t)addr > 0xfdff);
     asm("push _P_SW2");
     asm("orl _P_SW2,#80h");
     asm("nop");
