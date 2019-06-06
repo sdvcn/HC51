2a6a1731a1b6ba17d619b84eb09d250ce34f7263
sdvcn <sdvcn@126.com>
2019-06-06 17:01:00 +0800

    Create w281x.c
    w281x系列控制函数

---
+++ src/ext/w281x.c
@@ -0,0 +1,74 @@
+#include <stcmcu.h>
+
+
+#define	___mkstr1(x)	#x
+#define	___mkstr(x)	___mkstr1(x)
+
+#define ASM_W281xIO _P7.6
+#define ASM_W281xIOH()  asm("SETB   " ___mkstr(ASM_W281xIO) )
+#define ASM_W281xIOL()  asm("CLR    " ___mkstr(ASM_W281xIO) )
+#define ASM_W281xIOC()  asm("MOV    " ___mkstr(ASM_W281xIO) ",C")
+
+void w281writes( volatile const char* src,unsigned char len)
+{
+    //volatile unsigned char c = 0x00;
+
+    //SetDPS()
+    
+
+    asm("PUSH   _IE");
+    asm("CLR    _EA");
+    asm("MOV     _TA,#0AAH");
+    asm("MOV     _TA,#55H");
+    asm("MOV     _DPS,#00001000B");
+    asm("NOP");
+    //src++;
+    //if(*src){};
+    asm("MOV    DPL,R6");
+    asm("MOV    DPH,R7");
+
+    //asm("MOV    DPTR,w281writes@src");
+
+    //asm("WLOOP1:");
+    do{
+        //c = *src++;
+        //asm("MOV    R3,writes@c");
+        //asm("MOV    A,#0fh");
+        asm("MOVX    A,@DPTR");
+        asm("MOV    R2,#8");
+
+        asm("WLOOP2:");
+            asm("JC     WLOOP3");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("WLOOP3:");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            ASM_W281xIOH();
+            asm("RLC    A");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            ASM_W281xIOC();
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            asm("NOP");
+            ASM_W281xIOL();
+        asm("DJNZ R2,WLOOP2");
+
+    //asm("DJNZ R5,WLOOP1");
+    }while(--len);
+    asm("POP    _IE");
+}
