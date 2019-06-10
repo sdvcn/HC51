4604242aefd20ea9a54dcdd3799f574794e3e1ea
sdvcn <sdvcn@126.com>
2019-06-10 12:55:46 +0800

    Update w281x.c
    模拟方式实现w281x系列led控制

--- src/ext/w281x.c
+++ src/ext/w281x.c
@@ -1,4 +1,5 @@
 #include <stcmcu.h>
+#include <i2c.h>
 
 
 #define	___mkstr1(x)	#x
@@ -9,98 +10,51 @@
 #define ASM_W281xIOL()  asm("CLR    " ___mkstr(ASM_W281xIO) )
 #define ASM_W281xIOC()  asm("MOV    " ___mkstr(ASM_W281xIO) ",C")
 
-void w281writes( volatile const char* src,unsigned char len)
-{
-    //volatile unsigned char c = 0x00;
-
-    //SetDPS()
-    
-
-    asm("PUSH   _IE");
-    asm("CLR    _EA");
-    asm("MOV     _TA,#0AAH");
-    asm("MOV     _TA,#55H");
-    asm("MOV     _DPS,#00001000B");
-    asm("NOP");
-    //src++;
-    //if(*src){};
-    asm("MOV    DPL,R6");
-    asm("MOV    DPH,R7");
-
-    //asm("MOV    DPTR,w281writes@src");
-
-    //asm("WLOOP1:");
-    do{
-        //c = *src++;
-        //asm("MOV    R3,writes@c");
-        //asm("MOV    A,#0fh");
-        asm("MOVX    A,@DPTR");
-        asm("MOV    R2,#8");
-        unsigned char i = 8;
-        do{
-
-        }while(--i);
-
-        asm("WLOOP2:");
-            asm("JC     WLOOP3");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("WLOOP3:");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            ASM_W281xIOH();
-            asm("RLC    A");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            ASM_W281xIOC();
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            asm("NOP");
-            ASM_W281xIOL();
-        asm("DJNZ R2,WLOOP2");
-
-    //asm("DJNZ R5,WLOOP1");
-    }while(--len);
-    asm("POP    _IE");
-}
-
-void  wwa(const char* src,unsigned char len)
+/**
+ * 使用STC8扩展指针完成
+ * 工作时全局关闭中断
+*/
+void  w281xwrites(const char* src,unsigned char len)
 {
     asm("PUSH   _IE");
     asm("CLR    _EA");
     
     //asm("MOV    DPL1,R6");
     //asm("MOV    DPH1,R7");
+    //reset    
+    unsigned char l = 0xe0;
+    do{
+        ASM_W281xIOL();
+        asm("NOP2");
+        asm("NOP2");
+    }while(--l);
 
     DPH1 = ((size_t)src >> 8);
     DPL1 = (size_t)src;
+
     asm("MOV     _DPS,#00011001B");
 
     do{
         unsigned char i=8;
         //asm("MOVX    A,@DPTR");
         asm("CLR    A");
-        asm("movc	a,@a+dptr")
+        asm("movc	a,@a+dptr");
         do{
             ASM_W281xIOH();
             asm("RLC    A");
-            ASM_W281xIOC();
+            asm("NOP2");
+            asm("NOP2");
             asm("NOP");
-
+            //asm("JC ")
+            ASM_W281xIOC();
+            asm("CLR    C");
+            asm("NOP2");
+            asm("NOP2");
+            asm("NOP2");
+            asm("NOP2");
+            asm("NOP");
             ASM_W281xIOL();
+            asm("NOP");
         }while(--i);
     }while(--len);
     asm("MOV     _DPS,#00000000B");
